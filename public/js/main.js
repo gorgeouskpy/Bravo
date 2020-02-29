6affc16c617bf3ea77cc83804aed5774ce301174
gorgeouskpy <1284836331@qq.com>
2020-02-29 13:56:23 +0800

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+const topbar = {
+    template:
+    `
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 topbar">
+                <span class="top-1">广场</span>
+                <span class="top-2">社区</span>
+                <span class="top-3">个人中心</span>
+            </div>
+        </div>
+    </div>
+    `,
+};
