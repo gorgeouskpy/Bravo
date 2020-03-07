77b5df33161c6fb2d111490fb6393658d6d44025
gorgeouskpy <1284836331@qq.com>
2020-03-07 13:31:47 +0800

    username & password verify @ fontend

--- public/test.php
+++ public/test.php
@@ -1,3 +1,12 @@
 <?php
-echo sha1('123456');
+$process = 36**6;
+$startTime = microtime(true);
+echo "开始时间：".$startTime."\n";
+for($i=0;$i<$process;$i++){
+    sha1($i);
+}
+$endTime = microtime(true);
+$interval = $endTime - $startTime;
+echo "结束时间：".$endTime."\n";
+echo "过程：".$interval."\n";
 ?>
